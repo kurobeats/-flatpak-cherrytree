1a9fb1f6ee0f2d38c76c1359df1dd85d4f0ff2e9
kurobeats <kurobeats@users.noreply.github.com>
2019-09-30 18:57:41 +0800

    Delete package.sh

--- package.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-flatpak-builder --force-clean --arch=x86_64 --repo=repo/ build-dir/ com.giuspen.CherryTree.yaml
-flatpak build-bundle --arch=x86_64 repo/ CherryTree-0.38.8.x86_64.flatpak com.giuspen.CherryTree
-
